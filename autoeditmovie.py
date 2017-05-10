cc1f768077244e49cebcc6f6a2cbb222bcaa1b02
Chris <electronicsleep@gmail.com>
2017-05-09 19:48:21 -0700

    PEP 8

--- autoeditmovie.py
+++ autoeditmovie.py
@@ -5,25 +5,22 @@
 # Purpose: Learning MoviePy: Mirror, Edit and Concatination
 # Released under the BSD license
 
-# Running:
-# Usage: python autoeditmovie.py -m moviefile.mp4
-
-# Future: python autoeditmovie.py [-m] [-e] [-c]
-
-# WRAPPER SCRIPT FOR THE MOVIEPY PYTHON LIBRARY
-
-# SET SOURCE AND DESINATIONS OF MOVIE FILES
-sdir = "Camera1/"
-ddir = "Camera1/MoviePyOutput/"
-
-# CREATE DIRS AND PROCESS VIDEO FILES
-
-import errno    
+import errno
 import sys
 import os
 from moviepy.editor import *
 
-# MAKE DIRS IF THEY ARE NOT PRESENT
+# Running:
+# Usage: python autoeditmovie.py -m moviefile.mp4
+# Future: python autoeditmovie.py [-m] [-e] [-c]
+
+# Set source and destination
+sdir = "Camera1/"
+ddir = "Camera1/MoviePyOutput/"
+
+# Create dir and process video files
+
+# Make dir if they are not present
 
 def mkdir_p(path):
     try:
@@ -37,130 +34,126 @@ def mkdir_p(path):
 mkdir_p(sdir)
 mkdir_p(ddir)
 
-# CHECK FOR ARGS AND GIVE USAGE
+# Check for args and give usage
 
 total = len(sys.argv)
 cmdargs = str(sys.argv)
 
-#print ("The total numbers of args passed to the script: %d " % total)
-#print ("Args list: %s " % cmdargs)
+# print ("The total numbers of args passed to the script: %d " % total)
+# print ("Args list: %s " % cmdargs)
 
 try:
-  sys.argv[2]
+    sys.argv[2]
 except IndexError: 
-  print "Usage: autoeditmovie.py -m moviefile.mp4"
-  sys.exit(0)
+    print "Usage: autoeditmovie.py -m moviefile.mp4"
+    sys.exit(0)
 else:
-  print "Checking movie file"
+    print "Checking movie file"
 
 for arg in sys.argv: 
- print ("Argument: %s" % str(arg))
+    print ("Argument: %s" % str(arg))
 
 videofile = sys.argv[2]
 
 print ("Videofile %s" % str(videofile))
 
-### START MIRROR EFFECT
+# Start mirror effect
 
 if sys.argv[1] == '-m':
+    from moviepy.editor import VideoFileClip, clips_array, vfx
+    # add 10px contour
+    clip1 = VideoFileClip(sdir + videofile).margin(10)
+    clip2 = clip1.fx(vfx.mirror_x)
+    clip3 = clip1.fx(vfx.mirror_y)
+    clip4 = clip1.fx(vfx.mirror_y)
+    clip4 = clip4.fx(vfx.mirror_x)
+    # clip4 = clip1.resize(0.60) # downsize 60%
+    final_clip = clips_array([[clip1, clip2],
+                        [clip3, clip4]])
+    final_clip.resize(width=1280).write_videofile(ddir + "mirror.mp4")
 
- from moviepy.editor import VideoFileClip, clips_array, vfx
- clip1 = VideoFileClip(sdir + videofile).margin(10) # add 10px contour
- clip2 = clip1.fx( vfx.mirror_x)
- clip3 = clip1.fx( vfx.mirror_y)
- clip4 = clip1.fx( vfx.mirror_y)
- clip4 = clip4.fx( vfx.mirror_x)
- #clip4 = clip1.resize(0.60) # downsize 60%
- final_clip = clips_array([[clip1, clip2],
-                          [clip3, clip4]])
- final_clip.resize(width=1280).write_videofile(ddir + "mirror.mp4")
+# End mirror effect
 
-
-### END MIRROR EFFECT
-
-
-### START EDIT MOVIE EDITS ALL IN ONE DIR CURRENTLY
+# Start edit movie edits all in one dir currently
 
 if sys.argv[1] == '-e':
 
- os.listdir(sdir)
+    os.listdir(sdir)
+    from os import listdir
+    from os.path import isfile, join
+    onlyfiles = [f for f in listdir(sdir) if isfile(join(sdir, f))]
 
- from os import listdir
- from os.path import isfile, join
- onlyfiles = [ f for f in listdir(sdir) if isfile(join(sdir,f)) ]
+    mp4files = []
+    print "FILES"
+    for file in onlyfiles:
+        if file.endswith("MP4"):
+            print(file)
+            mp4files.append(file)
+            print("MP4 FILES")
 
+        for file in mp4files:
+            print file
 
- mp4files = []
- print "FILES"
- for file in onlyfiles:
-  #print file
-  if file.endswith("MP4"):
-   print file
-   mp4files.append(file)
-
- print "MP4 FILES"
-
- for file in mp4files:
-  print file
-
-  # Load mp4 and select the subclip 
-  clip = VideoFileClip(sdir + file).subclip(00,10)
+    # Load mp4 and select the subclip
+    clip = VideoFileClip(sdir + file).subclip(00,10)
  
-  # Reduce the audio volume (volume x 0.8)
-  #clip = clip.fx( afx.volumex, 0.8)
+    # Reduce the audio volume (volume x 0.8)
+    # clip = clip.fx( afx.volumex, 0.8)
 
-  # Add logo to movie
-  #clip = ImageClip(ddir + "logo.jpg")
-  #clip = ImageClip(100 100) # a (height x width x 3) RGB numpy array
-  #clip = video.to_ImageClip(t='00:00:00') # frame at t=1 hour.
-  #clip = video.to_ImageClip(t='01:00:00') # frame at t=1 hour.
+    # Add logo to movie
+    # clip = ImageClip(ddir + "logo.jpg")
+    # clip = ImageClip(100 100) # a (height x width x 3) RGB numpy array
+    # clip = video.to_ImageClip(t='00:00:00') # frame at t=1 hour.
+    # clip = video.to_ImageClip(t='01:00:00') # frame at t=1 hour.
 
-  #clip = ImageClip(ddir + "logo.jpg", transparent=False) # True is the default
-  #clip.mask # the alpha layer of the picture.
+    # clip = ImageClip(ddir + "logo.jpg", transparent=False) # True is the default
+    # clip.mask # the alpha layer of the picture.
  
-  # Generate a text clip. You can customize the font, color, etc.
-  txt_clip = TextClip("MEMORY ECHO",fontsize=200,color='black')
+    # Generate a text clip. You can customize the font, color, etc.
+    txt_clip = TextClip("MEMORY ECHO",fontsize=200,color='black')
  
-  # Say that you want it to appear 10s at the center of the screen
-  txt_clip = txt_clip.set_pos('bottom').set_duration(10)
+    # Say that you want it to appear 10s at the center of the screen
+    txt_clip = txt_clip.set_pos('bottom').set_duration(10)
  
-  # Overlay the text clip on the first video clip
-  video = CompositeVideoClip([clip, txt_clip])
+    # Overlay the text clip on the first video clip
+    video = CompositeVideoClip([clip, txt_clip])
  
-  # Write the result to a file
-  video.to_videofile(ddir + file ,fps=24, codec='mpeg4', audio=False)
+    # Write the result to a file
+    video.to_videofile(ddir + file ,fps=24, codec='mpeg4', audio=False)
 
-### END EDIT MOVIE
+# End edit move
 
 
-### START CONCATITNATE CLIPS EFFECT
+# Start concatenate clips effect
 
 if sys.argv[1] == '-c':
 
- print "### CONCAT CLIPS ###"
- onlyfiles = [ f for f in listdir(ddir) if isfile(join(ddir,f)) ]
+    print "### CONCAT CLIPS ###"
+    onlyfiles = [ f for f in listdir(ddir) if isfile(join(ddir,f)) ]
 
- mp4files = []
- print "OUTPUT FILES"
- for file in onlyfiles:
-  if file.endswith("MP4"):
-   print file
-   mp4files.append(file)
+    mp4files = []
+    print "OUTPUT FILES"
+    for file in onlyfiles:
+        if file.endswith("MP4"):
+            print file
+            mp4files.append(file)
 
- num=0
- for file in mp4files:
-  print "CONCAT FILE" + file
-  num = num + 1
+    num = 0
+    for file in mp4files:
+         print("CONCAT FILE" + file)
+         num = num + 1
+         print(file)
 
- #from moviepy.editor import VideoFileClip, concatenate_videoclips
- #clip1 = VideoFileClip(ddir + "videoexample.MP4")
- #clip2 = VideoFileClip(ddir + "videoexample.MP4")
- #clip3 = VideoFileClip(ddir + "videoexample.MP4")
+    # from moviepy.editor import VideoFileClip, concatenate_videoclips
+    # clip1 = VideoFileClip(ddir + "videoexample.MP4")
+    # clip2 = VideoFileClip(ddir + "videoexample.MP4")
+    # clip3 = VideoFileClip(ddir + "videoexample.MP4")
 
- #BUILD ALL
- final_clip = concatenate_videoclips([clip1,clip2,clip3])
- final_clip.write_videofile(ddir + "concatenation.mp4")
+    # Build All
+    final_clip = concatenate_videoclips([clip1,clip2,clip3])
+    final_clip.write_videofile(ddir + "concatenation.mp4")
 
-### END CONCATITNATE CLIPS EFFECT
 
-sys.exit(0)
+# End concatenate clips effect
+
+exit(0)
